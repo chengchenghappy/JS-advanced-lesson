b8a82fdd53c8e40da5ca3ed4b69a0723cb2ddd58
chengchenghappy <1306073684@qq.com>
2018-04-17 20:36:11 +0800

    LS13

---
+++ LS13/demo05.js
@@ -0,0 +1,11 @@
+var obj = {};
+obj.x = 2;//直接添加属性
+console.log(obj.x);//通过.访问属性
+obj.x = 5;//设置属性
+console.log(obj["x"]);//通过[]访问属性
+delete obj.x;//删除属性
+console.log(obj.x);
+
+//2
+// 5
+//对于有的属性就只能用[]来访问属性
